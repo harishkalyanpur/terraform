62f637f39af865f1d29e9c86b70c74513df8f395
harishkalyanpur <45255320+harishkalyanpur@users.noreply.github.com>
2020-09-02 08:16:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,23 @@ output "greeting" {
   }
 
 provider "random" {}
+
+locals {
+  //Arithmetic
+  three = 1+ 2
+  two = 3-1
+  one = 2/2
+  zero = 1*0
+  
+  // Logical
+  t = true || false
+  f = true && false
+  
+  //Comparison
+  gt = 2>1
+  gte = 2>=1
+  lt = 1<2
+  lte = 1<=2
+  eq = 1==1
+  neq = 1!=2
+  }
